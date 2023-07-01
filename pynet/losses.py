0ff3016f9dd97ac3508f397a1ac3253f916eb977
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-07-01 16:10:04 +0000

    add: SSIMLoss

---
+++ pynet/losses.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from keras.utils import losses_utils
+from tensorflow import keras
+
+
+class SSIMLoss(keras.losses.Loss):
+    def __init__(
+        self,
+        max_val: float = 1.0,
+        reduction=keras.utils.losses_utils.ReductionV2.AUTO,
+        name=None,
+    ):
+        self.max_val = max_val
+        super().__init__(reduction, name)
+
+    def call(self, y_true, y_pred):
+        return tf.reduce_mean(tf.image.ssim(y_true, y_pred, max_val=self.max_val))
