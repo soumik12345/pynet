73a810faa7c16a51c4ed13e948f1c72db4339c81
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-07-02 17:49:29 +0000

    add: MultiScaleSSIMLoss

--- pynet/losses.py
+++ pynet/losses.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from keras.utils import losses_utils
 from tensorflow import keras
 
 
@@ -15,3 +14,19 @@ class SSIMLoss(keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         return tf.reduce_mean(tf.image.ssim(y_true, y_pred, max_val=self.max_val))
+
+
+class MultiScaleSSIMLoss(keras.losses.Loss):
+    def __init__(
+        self,
+        max_val: float = 1.0,
+        reduction=keras.utils.losses_utils.ReductionV2.AUTO,
+        name=None,
+    ):
+        self.max_val = max_val
+        super().__init__(reduction, name)
+
+    def call(self, y_true, y_pred):
+        return tf.reduce_mean(
+            tf.image.ssim_multiscale(y_true, y_pred, max_val=self.max_val)
+        )
