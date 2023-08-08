22448248750658905314714a3fc7c7227113794b
Sanat B Singh <sanat.b.singh99@gmail.com>
2023-08-08 16:50:46 +0000

    dev: fixing installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,4 @@ sudo pip install -e .
 sudo pre-commit install
 sudo pip install namex
 sudo apt install python3.10-venv
-sudo git clone https://github.com/keras-team/keras-core.git && cd keras-core && python pip_build.py --install
+sudo git clone https://github.com/keras-team/keras-core.git && cd keras-core && sudo pip install -r requirements.txt && sudo python pip_build.py --install
