e318dc92672d0eb7d3956755497e4af5c478f46d
Sanat B Singh <sanat.b.singh99@gmail.com>
2023-08-08 16:30:56 +0000

    dev: testing keras-core installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,4 +2,6 @@ sudo pip install --upgrade pip
 sudo pip install --upgrade tensorflow==2.11.0
 sudo pip install -e .
 sudo pre-commit install
-sudo pip install -q keras-core
+sudo pip install namex
+sudo apt install python3.10-venv
+sudo git clone https://github.com/keras-team/keras-core.git && cd keras-core && python pip_build.py --install
